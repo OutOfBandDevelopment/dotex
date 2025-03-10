e99e26a7592ceb6ea76f7085c5de55db30b1d59d
Matthew Whited <matt@whited.us>
2025-03-10 18:09:25 -0400

    make embedding reader configurable

--- src/Extensions/OoBDev.Data.Vectors.Hosting/ServiceCollectionExtensions.cs
+++ src/Extensions/OoBDev.Data.Vectors.Hosting/ServiceCollectionExtensions.cs
@@ -1,12 +1,22 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 
 namespace OoBDev.Data.Vectors.Hosting;
 
 public static class ServiceCollectionExtensions
 {
-    public static IServiceCollection TryAddVectorHosting(this IServiceCollection services)
+    public static IServiceCollection TryAddVectorHosting(this IServiceCollection services,
+        IConfiguration configuration,
+#if DEBUG
+        string configurationSectionName
+#else
+        string configurationSectionName = nameof(EmbeddingSentenceTransformerQueueReaderOptions)
+#endif
+        )
     {
+        services.Configure<EmbeddingSentenceTransformerQueueReaderOptions>(options => configuration.Bind(configurationSectionName, options));
+
         services.TryAddTransient<IEmbeddingSentenceTransformerQueueReader, EmbeddingSentenceTransformerQueueReader>();
         services.AddHostedService<EmbeddingSentenceTransformerQueueReaderHost>();
         return services;
