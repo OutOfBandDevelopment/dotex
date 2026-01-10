85a18c0c65d5f58cba95e266b3e79debaf7a78c4
Matthew Whited <matt@whited.us>
2026-01-10 13:04:34 -0500

    phase 1

---
+++ Incomming/dotnet-lib/Framework/Eliassen.System.Abstractions/ComponentModel/Search/IgnoreStringComparisonReplacementAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Eliassen.System.ComponentModel.Search;
+
+/// <summary>
+/// exclude from string comparison replacement visitor
+/// </summary>
+[AttributeUsage(AttributeTargets.Property)]
+public class IgnoreStringComparisonReplacementAttribute : Attribute
+{
+}
